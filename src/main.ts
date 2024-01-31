761c1735d5b0315b2091c3ddb3a2ec27881b4a2a
Bartosz Utratny <utratny.bartosz@gmail.com>
2024-01-31 17:20:03 +0100

    Finished the website

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-const input = document.getElementById("email");
-if (input != null) {
-  console.log("OK!");
-  onfocus = () => {
-    input.style.border = "1px solid red"; // ! Placeholder
+const inputs = document.querySelectorAll("input, select, textarea");
+const errorMessage = document.getElementById("error__message");
+inputs.forEach(input => {
+  input.addEventListener("invalid", () => {
+    input.classList.add("error");
+    if (errorMessage != null) {
+      errorMessage.style.display = "block";
+    } else {
+      console.warn("errorMessage == null");
+    }
+  },
+  false);
+});
+
+document.addEventListener('invalid', (function(){
+  return function(e) {
+    e.preventDefault();
   };
-} else {
-  console.warn("Not OK!");
-}
+})(), true);
